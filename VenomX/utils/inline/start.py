92e1b25defc66655d21795e01224ad8fa5816155
eko777e <replitserver445@gmail.com>
2025-08-20 14:22:51 +0400

    Update start.py

--- VenomX/utils/inline/start.py
+++ VenomX/utils/inline/start.py
@@ -1,92 +1,41 @@
-
-# All rights reserved.
-#
-from typing import Union
-
 from pyrogram.types import InlineKeyboardButton
+import config
+from InflexMusic import app
 
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
-from VenomX import app
 
-
-def start_pannel(_):
+def start_panel(_):
     buttons = [
+        [ 
+            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_GROUP),
+        ],
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
+                text=_["S_B_5"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
-        ],
+        ]
     ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
-            )
-        if SUPPORT_GROUP:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
-            )
     return buttons
 
 
-def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def private_panel(_):
     buttons = [
-        [InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper")]
-    ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
-            )
-        if SUPPORT_GROUP:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
-            )
-    buttons.append(
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
             )
-        ]
-    )
-    if GITHUB_REPO and OWNER:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
-            ]
-        )
-    else:
-
-        if GITHUB_REPO:
-            buttons.append(
-                [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
-                ]
-            )
-
-        if OWNER:
-            buttons.append(
-                [
-                    InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                ]
-            )
-    buttons.append([InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")])
+        ],
+        [  
+            InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_3"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_GROUP),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_7"], user_id=config.OWNER_ID),
+        ],
+    ]
     return buttons
