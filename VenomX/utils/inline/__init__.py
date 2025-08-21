6538775e3c077717e8780bd7e1b16dbe7d1107c8
eko777e <replitserver445@gmail.com>
2025-08-22 00:37:25 +0400

    Update __init__.py

--- VenomX/utils/inline/__init__.py
+++ VenomX/utils/inline/__init__.py
@@ -1,10 +1,17 @@
-
+#
+# Copyright (C) 2024-2025 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
 # All rights reserved.
 #
+
 from .help import *
 from .play import *
 from .playlist import *
 from .queue import *
 from .settings import *
 from .song import *
-from .start import *
+from .start import private_panel, start_panel  # ❗ Burada 'start_pannel' yox, 'start_panel' düzgün formadır
